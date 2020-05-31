56e1179e12ec037863766e49f05b3e50738fb013
luk_96 <luk_96@mail.ru>
2020-05-31 16:41:48 +0300

    добавил IConst и число e

---
+++ MathParser/IConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathParser
+{
+    interface IConst
+    {
+        string Name { get;}
+
+        double Value { get;}
+    }
+}
